411fe5d41eb43603bfa2dc115f9a097cb5e7ff97
sonambharti <subodh9955509176@gmail.com>
2024-08-09 20:43:33 +0530

    Updated EMail code

--- BasicSyntax/node_email.js
+++ BasicSyntax/node_email.js
@@ -2,26 +2,35 @@
 require('dotenv').config();
 var nodemailer = require('nodemailer');
 
-password = process.env.NODEMAILER_PASS;
+const password = process.env.NODEMAILER_PASS;
+const email = process.env.NODEMAILER_email;
+const to_email = process.env.NODEMAILER_toemail;
+
 console.log('password = ', password);
+console.log('email = ', email);
+console.log('to_email = ', to_email);
+
 var transporter = nodemailer.createTransport({
-    service: 'gmail',
+    // host: 'smtp.gmail.com',
+    // port: 465,
+    // secure: true,
+    service: 'Gmail',
     auth: {
-        user: '2021pcs2040@iitjammu.ac.in',
+        user: email,
         pass: password
     }
 });
 
 var mailOptions = {
-    from: '2021pcs2040@iitjammu.ac.in',
-    to: '202117014@daiict.ac.in',
+    from: email,
+    to: to_email,
     subject: 'Sending Email using Node.js',
     text: 'Today I have a interview and I am practising.'
 };
 
 transporter.sendMail(mailOptions, function(error, info) {
     if (error) {
-        console.log(error);
+        console.log("There is an error:\n", error);
     } else {
         console.log('Email sent: ' + info.response);
     }
