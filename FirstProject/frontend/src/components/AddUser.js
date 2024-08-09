445e09242ca381860704a1c108bcfbd551af39ba
SONAM BHARTI <42584228+sonambharti@users.noreply.github.com>
2024-08-09 21:04:55 +0530

    First Basic MERN Project Added

---
+++ FirstProject/frontend/src/components/AddUser.js
@@ -0,0 +1,41 @@
+import React, { useState } from 'react';
+import axios from 'axios';
+
+const AddUser = () => {
+  const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    axios.post('http://localhost:5000/users', { name, email, password })
+      .then(response => console.log(response))
+      .catch(error => console.error(error));
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <input
+        type="text"
+        placeholder="Name"
+        value={name}
+        onChange={(e) => setName(e.target.value)}
+      />
+      <input
+        type="email"
+        placeholder="Email"
+        value={email}
+        onChange={(e) => setEmail(e.target.value)}
+      />
+      <input
+        type="password"
+        placeholder="Password"
+        value={password}
+        onChange={(e) => setPassword(e.target.value)}
+      />
+      <button type="submit">Add User</button>
+    </form>
+  );
+};
+
+export default AddUser;
