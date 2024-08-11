561bc61a304297e99bbf37a1e606d5a2c24df1c2
sonambharti <subodh9955509176@gmail.com>
2024-08-11 19:59:44 +0530

    MongoDB connected

--- FirstProject/backend/src/index.js
+++ FirstProject/backend/src/index.js
@@ -15,19 +15,6 @@ const connectDB = require("./db/mongoose");
 
 connectDB();
 
-
-// MongoDB Connection
-mongoose.connect('mongodb://localhost:27017/mern-basic', {
-  useNewUrlParser: true,
-  useUnifiedTopology: true
-});
-
-mongoose.connection.once('open', () => {
-  console.log('Connected to MongoDB');
-}).on('error', (error) => {
-  console.log('MongoDB connection error: ', error);
-});
-
 // Routes
 const usersRoute = require('./routes/users');
 app.use('/users', usersRoute);
