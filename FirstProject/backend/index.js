1d1abeea58ea856f27bec2783c98596db48c0f6d
kartikayt <kartikayt7@gmail.com>
2024-08-11 20:19:08 +0530

    working code

--- FirstProject/backend/index.js
+++ FirstProject/backend/index.js
@@ -1,28 +1,20 @@
-const express = require('express');
-const mongoose = require('mongoose');
-const bodyParser = require('body-parser');
-const cors = require('cors');
+const express = require("express");
+const connectDB = require("./db");
+const cors = require("cors");
 
-const app = express();
 const PORT = process.env.PORT || 5000;
 
+const app = express();
+
 // Middleware
-app.use(bodyParser.json());
 app.use(cors());
+app.use(express.json());
 
-// MongoDB Connection
-mongoose.connect('mongodb://localhost:27017/mern-basic', {
-  useNewUrlParser: true,
-  useUnifiedTopology: true
-});
-
-mongoose.connection.once('open', () => {
-  console.log('Connected to MongoDB');
-});
+connectDB();
 
 // Routes
-const usersRoute = require('./routes/users');
-app.use('/users', usersRoute);
+const usersRoute = require("./routes/users");
+app.use("/users", usersRoute);
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
