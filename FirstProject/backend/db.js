fc1afd20bb8f9c659e9a8042daaeb297c933b5eb
sonambharti <subodh9955509176@gmail.com>
2024-08-11 20:53:34 +0530

    Working Basic code

--- FirstProject/backend/db.js
+++ FirstProject/backend/db.js
@@ -3,7 +3,7 @@ const mongoose = require("mongoose");
 const connectDB = async () => {
   try {
     const conn = await mongoose.connect(
-      `mongodb://127.0.0.1:27017/firstprogram`
+      `mongodb://127.0.0.1:27017/mern-basic`
     );
     console.log(`MongoDB Connected:` + conn.connection.host);
   } catch (error) {
