a8229f2f9291a9d91c3cdd55dca69dbc6d9e1b28
sonambharti <subodh9955509176@gmail.com>
2024-10-25 22:10:37 +0530

    Added CRUD operation on DB functionality

--- Node-Tutorials/4-Express-REST-API/controllers/tourController.js
+++ Node-Tutorials/4-Express-REST-API/controllers/tourController.js
@@ -1,138 +1,113 @@
-const fs = require('fs');
-
-const tours = JSON.parse(fs.readFileSync(`${__dirname}/../dev-data/data/tours-simple.json`));
-
-
-exports.checkId = (req, res, next, val) => {
-    const id = req.params.id*1;
-    if (id > tours.length - 1){
-        return res.status(400).json({
-            status: 'fail',
-            message: 'Invalid id',
-        });
-    }
-    next();
-};
-
-
-// user-defined middleware to check if particular details are missing in request body or not...
-exports.checkBody = (req, res, next) => {
-    if (!req.body.name || !req.body.price) {
-        return res.status(404).json({
-            status: 'fail',
-            message: 'Missing name or price in request body',
-        });
-    }
-    next();
-}
-
+const TourModel = require('./../model/tour_model');
+const mongoose = require('mongoose');
 
 // Getting all the entries present in the tour package
-exports.getAllTour =  (req, res) => {
-    res.status(200).json({
-        status: 'success',
-        results: tours.length,
-        data: {
-            tours: tours,
-        },
-    });
+exports.getAllTour = async (req, res) => {
+    try{
+        const tours = await TourModel.find();
+        res.status(200).json({
+            status: 'success',
+            results: tours.length,
+            data: {
+                tours: tours,
+            },
+        });
+
+    }
+    catch (err){
+        res.status(400).json({
+            status: 'fail',
+            message: `Error: ${err}`,
+        });
+
+    }
+    
 }
 
-
-
 // Getting a tour entry via it's id
-exports.getTour = (req, res) => {
-    // console.log(req.params);
-    console.log(req.requestedAt);
-    const id = req.params.id*1;
-    
-    const tour = tours.find((el) => el.id === id);
+exports.getTour = async (req, res) => {
 
-    // if (! tours.find((el) => el.id === id)){
-    if (! tour){
-        return res.status(400).json({
-            status: 'fail',
-            message: 'Invalid id',
+    try{
+        const tour = await TourModel.findById(req.params.id);
+        res.status(200).json({
+            status: 'success',
+            data: {
+                tour: tour,
+            },
         });
-    }
 
-    res.status(200).json({
-        status: 'success',
-        data: {
-            tour: tour,
-        },
-    });
+    }
+    catch (err){
+        res.status(400).json({
+            status: 'fail',
+            message: `Error: ${err}`,
+        });
+
+    }
 }   
 
 // Update Tour
-exports.updateTour = (req, res) => {
-    console.log(req.params);
-    const id = req.params.id*1;
-    const tour = tours.find((el) => el.id === id);
-
-    // if (! tours.find((el) => el.id === id)){
-    if (! tour){
-        return res.status(400).json({
-            status: 'fail',
-            message: 'Invalid id',
+exports.updateTour = async (req, res) => {
+    try{
+        const updatedTour = await TourModel.findByIdAndUpdate(req.params.id, req.body, {
+            new: true,
+            runValidators: true
         });
+        res.status(200).json({
+            status: 'success',
+            data: {
+                tours: updatedTour,
+            },
+        });
+
+    }
+    catch (err){
+        res.status(400).json({
+            status: 'fail',
+            message: `Error: ${err}`,
+        });
+
     }
 
-    res.status(200).json({
-        status: 'success',
-        message: 'Tour updated',
-        data: {
-            tour: 'Updated',
-        },
-    });
 }   
 
 
 // Delete Tour
-exports.deleteTour = (req, res) => {
-    console.log(req.params);
-    const id = req.params.id*1;
-    const tour = tours.find((el) => el.id === id);
-
-    // if (! tours.find((el) => el.id === id)){
-    if (! tour){
-        return res.status(400).json({
-            status: 'fail',
-            message: 'Invalid id',
+exports.deleteTour = async (req, res) => {
+    try{
+        const tour = await TourModel.findByIdAndDelete(req.params.id);
+        res.status(200).json({
+            status: 'success',
+            data: null,
         });
-    }
 
-    res.status(200).json({
-        status: 'success',
-        message: 'Tour deleted',
-        data: null,
-    });
+    }
+    catch (err){
+        res.status(400).json({
+            status: 'fail',
+            message: `Error: ${err}`,
+        });
+
+    }
 } 
 
 
 // Adding new entry in the file 
-exports.addTour = (req, res) => {
-    const newId = tours[tours.length - 1].id + 1;
-    const newTour = Object.assign({id: newId}, req.body); //assigning and concatenating id object to the body object in the request
-    tours.push(newTour);
-
-    fs.writeFile(
-        `${__dirname}/../dev-data/data/tours-simple.json`, 
-        JSON.stringify(tours),
-        (err) => {
-            if (err) {
-                console.log(err);
-            }
-            else{
-                res.status(201).json({
-                    status: 'success',
-                    data: {
-                        tour: newTour,
-                    },
-                });
-            }
-        }
-    );
-
+exports.addTour = async (req, res) => {
+    try {
+        const newTour = await  TourModel.create(req.body);
+        res.status(201).json({
+            status: 'success',
+            data: {
+                tour: newTour,
+            },
+        });
+    }
+    catch (err){
+        res.status(400).json({
+            status: 'fail',
+            message: 'Error: Invalid Data Sent',
+        });
+    }
 };
 
--- Node-Tutorials/4-Express-REST-API/routes/tourRoutes.js
+++ Node-Tutorials/4-Express-REST-API/routes/tourRoutes.js
@@ -3,19 +3,15 @@ const tourController = require('./../controllers/tourController');
 
 const router = express.Router();
 
-// param is a router's middleware which is used to set/check param values for the application
-// router.param('id',(req, res, next, val) => {
-//     console.log(`Value of Id is: ${val}`);
-//     next();
-// });
-
-// here, if id is not in the req.url then it will just return to the next otherwise it will call checkId
-router.param('id', tourController.checkId);
+// router
+//     .route('/')
+//     .get(tourController.getAllTour)
+//     .post(tourController.checkBody, tourController.addTour); // calling for middleware check first using middleware chaining.
 
 router
     .route('/')
     .get(tourController.getAllTour)
-    .post(tourController.checkBody, tourController.addTour); // calling for middleware check first using middleware chaining.
+    .post(tourController.addTour); 
 
 router
     .route('/:id')
